0478a102b96eb08b515d0182fef8ba31a24e1dda
Mridul Pentapalli <mridulp@gmail.com>
2021-11-22 17:17:17 +0530

    Testing reading an input file

--- AoC2021/Day1/Day1.cpp
+++ AoC2021/Day1/Day1.cpp
@@ -1,20 +1,27 @@
-// Day1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <filesystem>
+#include <vector>
+#include <string>
+#include <fstream>
 #include <iostream>
+
+using namespace std::filesystem;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector <std::string> values;
+	path inputFile(L"input.txt");
+
+	std::string line;
+	std::ifstream fin(inputFile);
+	while (getline(fin, line))
+	{
+		values.push_back(line);
+	}
+
+	for (auto&& v:  values)
+	{
+		std::cout << v << std::endl;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
