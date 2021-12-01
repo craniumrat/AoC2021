4f77277e580ba7d71013edd1c84849abbbf303f9
Mridul Pentapalli <mridulp@gmail.com>
2021-12-01 10:53:01 +0530

    day1 part 1 works

--- Rust/day1/src/main.rs
+++ Rust/day1/src/main.rs
@@ -1,3 +1,29 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("test.txt") {
+        let mut prev = 0;
+        let mut total = 0;
+        for line in lines {
+            if let Ok(value) = line {
+                let num: i32 = value.parse().unwrap();
+                if num > prev {
+                    total += 1;
+                }
+
+                prev = num;
+            }
+        }
+
+        println!("{}", total - 1);
+    }
+}
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
