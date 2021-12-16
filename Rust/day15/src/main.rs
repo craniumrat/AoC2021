58f042ec88ee4e8ef52815c861523d4b597a8db5
Mridul Pentapalli <mridulp@gmail.com>
2021-12-16 10:50:05 +0530

    Part 1 working

--- Rust/day15/src/main.rs
+++ Rust/day15/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use petgraph::graph::UnGraph;
+use petgraph::graph::DiGraph;
 use petgraph::algo::dijkstra;
 
 fn main() {
@@ -10,11 +10,11 @@ fn main() {
 
     let size = grid.len();
 
-    let start_node = graph.node_indices().find(|n| graph[*n] == (0, 0)).unwrap();
-    let end_node = graph.node_indices().find(|n| graph[*n] == (TryInto::<u32>::try_into(size).unwrap() - 1, TryInto::<u32>::try_into(size).unwrap() - 1)).unwrap();
+    let start_node = graph.node_indices().find(|n| graph[*n] == 0).unwrap();
+    let end_node = graph.node_indices().find(|n| n == &petgraph::prelude::NodeIndex::new(size * size - 1)).unwrap();
 
     let solution = dijkstra(&graph, start_node, Some(end_node), |e| *e.weight());
-    println!("{:?}", solution.get(&end_node));
+    println!("Part 1: {:?}", solution.get(&end_node));
 }
 
 fn make_grid() -> Vec<Vec<u32>>
@@ -29,7 +29,7 @@ fn make_grid() -> Vec<Vec<u32>>
                     row.push(c.to_digit(10).unwrap());
                 }
 
-                println!("{:?}", row);
+                // println!("{:?}", row);
                 grid.push(row)
             }
         }
@@ -38,39 +38,31 @@ fn make_grid() -> Vec<Vec<u32>>
     grid
 }
 
-fn make_graph(grid: &Vec<Vec<u32>>) -> UnGraph<(u32, u32), u32>
+fn make_graph(grid: &Vec<Vec<u32>>) -> DiGraph<u32, u32>
 {
     let size = grid.len();
-    let mut graph = UnGraph::<(u32, u32), u32>::with_capacity(size, size * size);
 
-    let mut node_indices = vec!();
-
-    for i in 0..size {
-        let mut node_indices_row = vec!();
-        for j in 0..size {
-            let n = graph.add_node((i.try_into().unwrap(), j.try_into().unwrap()));
-            node_indices_row.push(n);
-        }
-
-        node_indices.push(node_indices_row);
-    }
-
+    //Need to create a tuple of (source, target, weight) for all
+    //nodes. We call source & target as (i*size + j) u32's and weight as grid[i][j] of target
+    let mut edge_weights = vec!();   
+ 
     for i in 0..size {
         for j in 0..size {
             if i < size - 1 {
-                let start = node_indices[i][j];
-                let end = node_indices[i + 1][j];
-                graph.update_edge(start, end, grid[i + 1][j]);
+                edge_weights.push((TryInto::<u32>::try_into(i * size + j).unwrap(), TryInto::<u32>::try_into((i + 1) * size + j).unwrap(), TryInto::<u32>::try_into(grid[i + 1][j]).unwrap()));
+                edge_weights.push((TryInto::<u32>::try_into((i + 1) * size + j).unwrap(), TryInto::<u32>::try_into(i * size + j).unwrap(), TryInto::<u32>::try_into(grid[i][j]).unwrap()));
             }
 
             if j < size - 1 {
-                let start = node_indices[i][j];
-                let end = node_indices[i][j + 1];
-                graph.update_edge(start, end, grid[i][j + 1]);
+                edge_weights.push((TryInto::<u32>::try_into(i * size + j).unwrap(), TryInto::<u32>::try_into(i * size + j + 1).unwrap(), TryInto::<u32>::try_into(grid[i][j + 1]).unwrap()));
+                edge_weights.push((TryInto::<u32>::try_into(i * size + j + 1).unwrap(), TryInto::<u32>::try_into(i * size + j).unwrap(), TryInto::<u32>::try_into(grid[i][j]).unwrap()));
             }
         }
     }
 
+    // println!("{:?}", edge_weights);
+
+    let graph = DiGraph::from_edges(&edge_weights);
     graph
 }
 
@@ -93,12 +85,19 @@ fn test_make_graph() {
     let grid = make_grid();
     let graph = make_graph(&grid);
 
+    let size_u32: u32 = grid.len().try_into().unwrap();
+
     assert_eq!(graph.node_count(), grid.len() * grid.len());
 
-    let first_node = graph.node_indices().find(|i| graph[*i] == (0, 1)).unwrap();
-    for edge in graph.edges(first_node) {
-        println!("{}", edge.weight());
-    }
+    // for v in graph.node_indices() {
+    //     println!("{:?}", v);
+    // }
 
-    assert_eq!(1, 1);
+    // for v in graph.node_indices() {
+    //     for e in graph.edges(v) {
+    //         println!("{:?}", e);
+    //     }
+    // }
+
+    assert_eq!(1, 0);
 }
